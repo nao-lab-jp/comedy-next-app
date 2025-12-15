6b8a8cf90df71a6d76a4154b5bef01ff12fc637a
naohiro-comedy <naohiro.tsujioka@gmail.com>
2025-12-15 10:29:25 +0900

    OGP設定を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 // ▼ TypeScript用に型定義 (: Metadata) を追加しました
 export const metadata: Metadata = {
+  // サイトのドメイン（これを指定しておくと画像のURLなどが短く書けます）
   metadataBase: new URL("https://owarai-live.com"),
 
   title: {
@@ -28,12 +29,23 @@ export const metadata: Metadata = {
     siteName: "東京お笑いライブ検索",
     locale: "ja_JP",
     type: "website",
+    // ▼ 追加: 画像設定（publicフォルダのogp.pngを参照します）
+    images: [
+      {
+        url: "/ogp.png",
+        width: 1200,
+        height: 630,
+        alt: "東京お笑いライブ検索",
+      },
+    ],
   },
 
   twitter: {
     card: "summary_large_image",
     title: "東京お笑いライブ検索",
     description: "推しの芸人のライブ予定を今すぐチェック！",
+    // ▼ 追加: Twitter用の画像指定
+    images: ["/ogp.png"],
   },
 
   // ▼ Google Search Consoleの認証コード
